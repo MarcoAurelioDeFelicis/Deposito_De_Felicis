9c909b8980a4298c50511d13c8aaa7729166ae0f
MarcoAurelioDeFelicis <158585743+MarcoAurelioDeFelicis@users.noreply.github.com>
2026-02-05 16:21:14 +0100

    feat: aggiunta funzione di esempio per l'astrazione e rimozione di un file obsoleto

---
+++ Gio_05_02_2026/Esempio_func.py
@@ -0,0 +1,10 @@
+# La prova pratica dell'astrazione, sono blocchi di codice autonomi e scalanili e riutilizzabili
+# che eseguono un a azione specifica:
+# caratteristiche:
+# modularità = "modulo prefatto che può essere utilizzato in doversi progetti"
+# manutenibili = "modificare qualcosa che viene richiamata in varie parti"
+
+def impreca(dude, divinità):
+    print(f"{dude} dice: P***+{divinità}!!")
+    
+impreca("Marco", "Anubi")
