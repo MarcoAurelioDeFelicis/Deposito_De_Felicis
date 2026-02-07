3556fa1017a7b68a8fc7da87a40089b5161cf178
MarcoAurelioDeFelicis <158585743+MarcoAurelioDeFelicis@users.noreply.github.com>
2026-02-07 20:57:59 +0100

    rimosso il try except in popola

--- Esercizi_fine_settimana/Prove.py
+++ Esercizi_fine_settimana/Prove.py
@@ -12,3 +12,16 @@ elenco_tabelle = {
 # print(elenco_tabelle.values())
 nome_tab = "utenti"
 print(elenco_tabelle.get(nome_tab))
+
+
+  
+  
+# TODO: mostra le colonne da schema_tabelle per tabella selezoinata,
+#procede alla creazione del record seguendo le regole dello schema, 
+#aggiungi un sistema di validazione per il tipo di dato da inserire,
+#salva il record in un dizionario db {Pizzeria: {
+    #"pizza":[
+        #["margherita","pomodoro,mozzarella",8.00],
+        #["diavola","pomodoro,salame,mozzarella",10.00]
+    # ]
+# }
