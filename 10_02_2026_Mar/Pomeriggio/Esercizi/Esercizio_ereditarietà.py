1992e20d224ccf512b204b83e4a4dcf5cbff9eba
MarcoAurelioDeFelicis <158585743+MarcoAurelioDeFelicis@users.noreply.github.com>
2026-02-11 01:56:45 +0100

    add: categoria param in Prodotto, def zero_checker per controllo e drop degli items unita 0 in magazzino e reso decoratore. , TO FIX: def sker che usa inspect per prendere e chiedere i metodi da passare come parametr a def aggiungi_prodotto

--- 10_02_2026_Mar/Pomeriggio/Esercizi/Esercizio_ereditarietà.py
+++ 10_02_2026_Mar/Pomeriggio/Esercizi/Esercizio_ereditarietà.py
@@ -16,6 +16,10 @@
 # vendi_prodotto: diminuisce la quantità di un prodotto in inventario e stampa il profitto realizzato dalla vendita.
 # resi_prodotto: aumenta la quantità di un prodotto restituito in inventario.
 # Decoratore
+import inspect
+from functools import wraps
+
+#Decoratore
 def log_func(funzione):
     def wrapper(*args, **kwargs):
         print(f"\n---> Run di funzione : {funzione.__name__}")
@@ -23,42 +27,62 @@ def log_func(funzione):
         return risultato
     return wrapper
 
+# --- CLASSE PRODOTTO  ---
 class Prodotto:
-    def __init__(self, nome: str, costo_produzione: float, prezzo_vendita: float, unita: int):
-        
+    def __init__(self, nome: str, costo_produzione: float, prezzo_vendita: float, unita: int, categoria:str):
         self.nome = nome
         self.costo_produzione = costo_produzione
         self.prezzo_vendita = prezzo_vendita
         self.unita = unita
+        self.categoria = categoria
         
     def calcola_profitto(self, quantita_da_vendere):
-        self.unita -= quantita_da_vendere
         profitto = self.prezzo_vendita * quantita_da_vendere - self.costo_produzione * quantita_da_vendere
-        print(f"{quantita_da_vendere} unita di  {self.nome} vendute, Rimanenti: {self.unita} un\n")
         print(f"profitto prodotto: {profitto}\n")
         print(f"costo di produzione * unita {self.costo_produzione * quantita_da_vendere}")
         return profitto 
-        
-class Elettronica(Prodotto):
-    def __init__(self, nome, costo_produzione, prezzo_vendita, unita, features: list, classe_energetica: str):
-        super().__init__(nome, costo_produzione, prezzo_vendita, unita)
+    
+    def __str__(self):
+        return f"PRODOTTO: '{self.nome}' | Prezzo: {self.prezzo_vendita}$ | Unità: {self.unita}"
 
+# --- CLASSE ELETTRONICA(Prodotto) ---
+class Elettronica(Prodotto):
+    def __init__(self, nome, costo_produzione, prezzo_vendita, unita, categoria, componenti: list, classe_energetica: str):
+        super().__init__(nome, costo_produzione, prezzo_vendita, unita, categoria)
+        self.componenti = componenti
+        self.classe_energetica = classe_energetica
+        
+    def __str__(self):
+        return f"(Categoria: {self.categoria}) " + super().__str__() 
+
+# --- CLASSE ABBIGLIAMENTO(Prodotto) ---
 class Abbigliamento(Prodotto):    
     
     taglie_disponibili = ("XL","L","M","S","XS" )
     
-    def __init__(self, nome, costo_produzione, prezzo_vendita, unita, materiale: str, taglia:str):
-        super().__init__(nome, costo_produzione, prezzo_vendita)
+    def __init__(self, nome, costo_produzione, prezzo_vendita, unita, categoria, materiale: str, taglia:str):
+        super().__init__(nome, costo_produzione, prezzo_vendita, unita, categoria)
         self.materiale = materiale
         self.taglia = taglia
+        
+    def __str__(self):
+        return f"(Categoria: {self.categoria}) " + super().__str__() 
+        
 
+# --- CLASSE FABBRICA ---
 class Fabbrica:
-    def __init__(self, inventario={}):
-        self.inventario = inventario
+    TIPI_PROD = {"elettronica": Elettronica, "abbigliamento": Abbigliamento}
+    
+    def __init__(self, nome, inventario=None):
+        if inventario is None:
+            inventario = {}
         self.storico_vendite = []
+        self.nome = nome
+        
+#-------------- METODI --------------------------------------------
         
     def stampa_inventario(self):
-        print(f"--- INVENTARIO DI {self.nome.lower()} ---")
+        print(f"--- INVENTARIO DI: {self.nome.lower()} ---")
         if not self.inventario:
             print("Il magazzino è vuoto.")
         else:
@@ -66,21 +90,87 @@ class Fabbrica:
             for p in self.inventario.values():
                 print(p) #__str__ di Pacco
         print("----------------------------------")
+    
+    @staticmethod   
+    def zero_checker(func):
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            #Run del metodo wrappato
+            result = func(self, *args, **kwargs)
+            if hasattr(self, 'inventario'):
+                self.inventario = {k: v for k, v in self.inventario.items() if v.unita > 0}
+            
+                # for k, v in self.inventario:
+                #     if v.unita <= 0: 
+                #         del self.inventario[k]
+            
+            return result
+        return wrapper
+            
+    def asker(self, classe_scelta):
+        ''' Trova i parametri necessari e chiede l'input all'utente '''
+        #firma del metodo __init__
+        parametri = inspect.signature(classe_scelta.__init__).parameters
         
-    @log_func   #def __init__(self, nome: str, costo_produzione: float, prezzo_vendita: float):
+        args_da_passare = []
+        
+        print(f"\n--- Configurazione {classe_scelta.name} ---")
+        for nome_param, param in parametri.items():
+            # Skip 'self'
+            if nome_param == 'self':
+                continue
+            
+            valore = input(f"Inserisci {nome_param}: ")
+            
+            # TODO : Qui potrei aggiungere una logica per convertire in int/float
+            # Per ora li lascio come stringhe o cast base
+            args_da_passare.append(valore)
+            
+        return args_da_passare
+    
+    @log_func
+    @zero_checker   
+    def crea_prodotto(self, classe_scelta, args_lista):
+        ''' Spacchetta * gli argomenti e istanzia OBJ della classe '''
+        try:
+            # L'operatore * trasforma la lista [a, b, c] in (a, b, c) passati come argomenti
+            new_OBJ = classe_scelta(*args_lista)
+            
+            # Lo aggiungiamo all'inventario usando il nome come chiave
+            self.inventario[new_OBJ.nome] = new_OBJ
+            print(f"SUCCESSO! {new_OBJ.nome} creato e aggiunto!")
+            return new_OBJ
+        except Exception as e:
+            print(f"ERRORE: durante la creazione: {e}")
+
+
+    @log_func   
     def aggiungi_prodotto(self, prodotto: Prodotto):
-        if not prodotto in self.inventario:
-            self.inventario[prodotto]
-            print(f"prodotto {prodotto.nome} aggiunto all'inventrio\n")
-            print(f"Inventario AGGIORNATO : {self.stampa_inventario()}")
+        
+        #crea_prodotto()
+        prodotto_OBJ = prodotto
+        if not prodotto_OBJ.nome in self.inventario:
+            self.inventario[prodotto_OBJ.nome] = prodotto_OBJ
+            print(f"prodotto {prodotto_OBJ.nome} aggiunto all'inventrio\n")
+            print(f"Inventario AGGIORNATO :")
+            
+            self.stampa_inventario()
         else:
             print("ERRORE")
             
-    @log_func        
-    def vendi_prodotto(self, prodotto: Prodotto, quantita_da_vendere: int):
-        if  prodotto in self.inventario and quantita_da_vendere <= prodotto.unita:
+    @log_func 
+    @zero_checker       
+    def vendi_prodotto(self, nome_prodotto: str, quantita_da_vendere: int):
+        # prodotto_OBJ = [i for i in self.inventario if i == nome_prodotto]
+        prodotto_OBJ = self.inventario.get(nome_prodotto)
+    
+        if prodotto_OBJ and quantita_da_vendere <= prodotto_OBJ.unita:
+            prodotto_OBJ.unita -= quantita_da_vendere
+            print(f"{quantita_da_vendere} unita di {prodotto_OBJ.nome} vendute, Rimanenti: {prodotto_OBJ.unita} un\n")
+            
+            # self.zero_checker()
             #TODO: se quantità scende a 0 allora viene rimosso da magazzino
-            vendita = prodotto.calcola_profitto(quantita_da_vendere)
+            vendita = prodotto_OBJ.calcola_profitto(quantita_da_vendere)
             return vendita
         else:
             print("ERRORE: Prodotto non trovato o quantità non valida.")
